8bdcd53d96cb506b9ac1efc04b43b3cb7c31cf99
astel <kiyomillefeuille@gmail.com>
2018-06-06 18:21:35 +0900

    case class化

--- src/main/scala/net/astail/Main.scala
+++ src/main/scala/net/astail/Main.scala
@@ -4,23 +4,25 @@ import scala.io.Source
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
+
 object Main {
   def main(args: Array[String]): Unit = {
-
     //val api = "https://spla2.yuu26.com/regular/now"
     val api = "https://astail.net/ika.html"
     val source = Source.fromURL(api, "utf-8")
     val str = source.getLines.mkString
     val jsonObj = parse(str)
-    val values = jsonObj.asInstanceOf[JObject].values
 
-    val result = values("result")
-    val resultTypes = result.asInstanceOf[List[Map[String,Any]]]
-    val maps_ex = resultTypes(0)("maps_ex")
-    val map = maps_ex.asInstanceOf[List[Map[String,Any]]]
+    implicit val formats = DefaultFormats
+    case class Maps_ex(id: Int, name: String, statink: String)
+    case class Rule_ex(key: String, name: String, statink: String)
+    case class Result(rule: String, rule_ex: Rule_ex, maps: List[String], maps_ex: List[Maps_ex],
+      start: String, start_utc: String, start_t: Int, end: String, end_utc: String, end_t: Int)
 
-    for(xx <- map) println(xx("name"))
+    val listResult = (jsonObj \ "result").extract[List[Result]]
+    val maps = listResult(0).maps.mkString(",")
 
+    println(maps)
     source.close
   }
 }
