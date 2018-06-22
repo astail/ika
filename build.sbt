9bba6ea95d06b96f57c6b4bfa8526f66a0305bc3
astel <kiyomillefeuille@gmail.com>
2018-06-22 17:13:10 +0900

    バイト募集中かをわかりやすく

--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ version := "0.1.0-SNAPSHOT"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
+  "com.github.nscala-time" %% "nscala-time" % "2.20.0",
   "com.typesafe" % "config" % "1.3.3",
   "org.json4s" %% "json4s-native" % "3.6.0-M3",
   "org.json4s" %% "json4s-jackson" % "3.6.0-M3",
--- src/main/scala/net/astail/Main.scala
+++ src/main/scala/net/astail/Main.scala
@@ -1,5 +1,7 @@
 package net.astail
 
+import org.joda.time.DateTime
+
 import scala.io.Source
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
@@ -7,6 +9,7 @@ import org.json4s.jackson.JsonMethods._
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val timestamp: DateTime = DateTime.now()
     ArgumentsParser.parse(args, Arguments()).foreach { a =>
       val time = a.time
       val battle = a.battle
@@ -34,32 +37,54 @@ object Main {
       }
     }
 
-
-    def coop(api: String, time: String) = {
-      val resultDataCoop1 = resultDataCoop(api, time)
-
-      def stage: String = resultDataCoop1.stage.name
-
-      def weapons = resultDataCoop1.weapons.map(_.name).mkString(",")
-      def sTime = resultDataCoop1.start
-      def eTime = resultDataCoop1.end
-
-
-
-      println(s"${sTime} ~ ${eTime}")
-      println(s"ステージ: ${stage}, 武器: ${weapons}")
+    def checkTime(s: String, e: String, now: DateTime): Boolean = {
+      val sTime = new DateTime(s)
+      val eTime = new DateTime(e)
+      now.isAfter(sTime) && now.isBefore(eTime)
     }
 
     def nomal(api: String, battle2: String, time2: String) = {
       val resultData1 = resultData(api)
 
       def map: String = resultData1.maps.mkString(",")
-
       def rule: String = resultData1.rule_ex.name
+      def sTime = resultData1.start
+      def eTime = resultData1.end
 
-      println(s"バトル: ${battle2}, 時間: ${time2}, ルール: ${rule}, マップ: ${map}")
+      println(s"バトル: ${battle2}\n時間: ${time2}, ${sTime} ~ ${eTime}\nルール: ${rule}\nマップ: ${map}")
     }
 
+    def coop(api: String, time: String) = {
+      val resultDataCoop1 = resultDataCoop(api, time)
+
+      def stage: String = resultDataCoop1.stage.name
+      def weapons = resultDataCoop1.weapons.map(_.name).mkString(",")
+      def image = resultDataCoop1.weapons.map(_.image).mkString("\n")
+      def sTime = resultDataCoop1.start
+      def eTime = resultDataCoop1.end
+
+      val kuma = if (checkTime(sTime,eTime,timestamp))
+        "バイト募集中"
+      else
+        "シフトを確認してくれたまえ"
+
+      println(s"${kuma}\n時間: ${sTime} ~ ${eTime}\nステージ: ${stage}\n武器: ${weapons}\n${image}")
+    }
+
+
+    def resultData(api: String): Result = {
+      val source = Source.fromURL(api, "utf-8")
+      val str = source.getLines.mkString
+      val jsonObj = parse(str)
+
+      try {
+        implicit val formats = DefaultFormats
+        val listResult = (jsonObj \ "result").extract[List[Result]]
+        listResult(0)
+      } finally {
+        source.close
+      }
+    }
 
     def resultDataCoop(api: String, time: String): ResultCoop = {
       val source = Source.fromURL(api, "utf-8")
@@ -75,20 +100,6 @@ object Main {
           case "next" => listResult(1)
           case _ => listResult(0)
         }
-      } finally {
-        source.close
-      }
-    }
-
-    def resultData(api: String): Result = {
-      val source = Source.fromURL(api, "utf-8")
-      val str = source.getLines.mkString
-      val jsonObj = parse(str)
-
-      try {
-        implicit val formats = DefaultFormats
-        val listResult = (jsonObj \ "result").extract[List[Result]]
-        listResult(0)
       } finally {
         source.close
       }
